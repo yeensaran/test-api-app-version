e725bd80bad558fdc167a6186352e6bf02e23ac6
yeensaran <saran@skilllane.com>
2025-12-12 15:04:07 +0700

    test version

--- packages/api/src/app.js
+++ packages/api/src/app.js
@@ -15,8 +15,6 @@ app.use(helmet());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-console.log("process.env.APP_VERSION: ",process.env.APP_VERSION || "3.0.1");
-
 const health = require('./routes/health');
 
 app.use('/api/health', health);
